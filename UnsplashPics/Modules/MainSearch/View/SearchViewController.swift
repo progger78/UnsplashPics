d144d543fa9486797c0f7cd8e35409b7369d1159
progger78 <pav_egor_24@mail.ru>
2025-02-05 12:27:14 +0300

    initial commit

---
+++ UnsplashPics/Modules/MainSearch/View/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  UnsplashPics
+//
+//  Created by 1 on 05.02.2025.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
---
+++ UnsplashPics/Utils/AppError.swift
@@ -0,0 +1,8 @@
+//
+//  AppError.swift
+//  UnsplashPics
+//
+//  Created by 1 on 05.02.2025.
+//
+
+import Foundation
